fd0e535a6ea3ed2ffb3a2f126974e4a784dc6043
Jason Axelson <jason.axelson@gmail.com>
2022-06-05 09:39:05 -1000

    Loosen version restriction

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule ScenicLiveReload.MixProject do
     [
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       dep(:exsync, :hex),
-      {:scenic, "~> 0.10 and <= 0.10.5"}
+      {:scenic, "~> 0.11.0-beta.0"}
     ]
   end
 
